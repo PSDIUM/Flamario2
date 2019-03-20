362ec10ce91081d8935de3d3581aa263e7ad74a5
rromano <rromanomusic@gmail.com>
2019-03-21 07:29:16 +1100

    Initial build, bringing in old project.

---
+++ Flamario2Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float speed; 
+    
+    void Update() {
+        Movement();
+    }
+
+	private void Movement() {
+		float input = Input.GetAxis("Horizontal");
+		if (input!=0) {
+			transform.position += new Vector3(input * speed * Time.deltaTime, 0, 0);
+		}
+	}
+}
