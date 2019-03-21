3e3312c84be4b2a3174475a6624b6784ec249678
Gunjoe169 <Gunjoe169@gmail.com>
2019-03-21 20:41:15 +1100

    Added the GameController

---
+++ Flamario2Unity/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Lock and hide the cursor
+        //SetCursorState(CursorLockMode.Locked);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+
+    public void SetCursorState(CursorLockMode wantedMode) {
+        Cursor.lockState = wantedMode;
+        // Hide cursor when locking
+        Cursor.visible = (CursorLockMode.Locked != wantedMode);
+    }
+    
+    public void QuitGame() {
+        #if UNITY_EDITOR  
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        #if UNITY_STANDALONE
+                Application.Quit();
+        #endif
+    }
+}
