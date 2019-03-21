43279e9f1492591d24739902ca39c211025442bf
Gunjoe169 <Gunjoe169@gmail.com>
2019-03-21 23:26:19 +1100

    Camera follow within range.
    The camera now follows within only a set range smoothly, however in both directions. A camera space border/collider has also been added.

--- Flamario2Unity/Assets/Scripts/CameraController.cs
+++ Flamario2Unity/Assets/Scripts/CameraController.cs
@@ -6,6 +6,10 @@ public class CameraController : MonoBehaviour {
 
     public GameObject target;
     private Vector3 offset;
+    public float leftBorderX;
+    public float rightBorderX;
+    public float smoothDampTime = 0.2F;
+    private float smoothDampVel = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,9 @@ public class CameraController : MonoBehaviour {
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = target.transform.position + offset;
+        //transform.position = target.transform.position + offset;
+        float targetX = Mathf.Max(leftBorderX, Mathf.Min(rightBorderX, target.transform.position.x));
+        float x = Mathf.SmoothDamp(transform.position.x, targetX, ref smoothDampVel, smoothDampTime);
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
     }
 }
