50fb4287727f8033d5af767f7fa2bcf97ea03f07
rromano <rromanomusic@gmail.com>
2019-03-24 10:44:36 +1100

    Created basic player jump mechanic and collision detection.

--- Flamario2Unity/Assets/Scripts/PlayerController.cs
+++ Flamario2Unity/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,73 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private int collisionLayer = 1<<9;
+	private PlayerStates playerState;
 	private float speed = 10;
-  
-    void Update() {
+	private float jumpSpeed = 15;
+	private float jumpVelocity = 0;
+	private float gravity = 20;
+
+	private void Start() {
+		playerState = PlayerStates.IDLE;
+	}
+
+	void Update() {
         Movement();
     }
 
     private void Movement() {
+		Run();
+		Jump();
+	}
+
+	private void Run() {
 		float hInput = Input.GetAxis("Horizontal");
 		if (hInput != 0) {
-			if (IsColliding(hInput)){
+			if (IsColliding(hInput < 0 ? Vector3.left : Vector3.right)) {
 				return;
-            }
+			}
 			transform.position += new Vector3(hInput * speed * Time.deltaTime, 0, 0);
 		}
-    }
+	}
 
-    private bool IsColliding(float input) {
-		Vector3 dir = input < 0 ? Vector3.left : Vector3.right;
+	private void Jump() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			if (playerState != PlayerStates.JUMPING) {
+				playerState = PlayerStates.JUMPING;
+				jumpVelocity = jumpSpeed;
+			}
+		}
+		if (playerState == PlayerStates.JUMPING) {
+			jumpVelocity -= gravity * Time.deltaTime;
+			transform.position += new Vector3(0, jumpVelocity * Time.deltaTime, 0);
+
+			if (IsColliding(Vector2.up)) {
+				jumpVelocity = 0;
+			} 
+			else if (IsColliding(Vector2.down)) {
+				playerState = PlayerStates.IDLE;
+				return;
+			}
+		}
+	}
+
+	private bool IsColliding(Vector2 dir) {
+		//Vector3 dir = input < 0 ? Vector3.left : Vector3.right;
 		Vector3 origin = transform.position;
 
 		RaycastHit2D hit = Physics2D.Raycast(origin, dir, transform.lossyScale.x/2, collisionLayer);
-		//Debug.DrawRay(origin, dir * transform.lossyScale.x/2, Color.yellow);
+		Debug.DrawRay(origin, dir * transform.lossyScale.x/2, Color.yellow);
 		return hit.collider != null;
     }
+
+	private bool IsFalling() {
+
+		return true;
+	}
+
+	private enum PlayerStates {
+		IDLE,
+		WALKING,
+		JUMPING
+	}
 }
