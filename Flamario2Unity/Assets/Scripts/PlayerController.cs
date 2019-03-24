7eca09ac03e70c5ea3ac13fc13017137a57b42b9
rromano <rromanomusic@gmail.com>
2019-03-24 16:14:46 +1100

    Merging player movement and colisions

--- Flamario2Unity/Assets/Scripts/PlayerController.cs
+++ Flamario2Unity/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    private int collisionLayer = 1<<9;
+	private int collisionLayer = 1 << 9;
 	private PlayerStates playerState;
+
+	private Collisions[] collisions;
 	private float speed = 10;
 	private float jumpSpeed = 15;
 	private float jumpVelocity = 0;
@@ -12,24 +14,52 @@ public class PlayerController : MonoBehaviour {
 
 	private void Start() {
 		playerState = PlayerStates.IDLE;
+		SetCollisions();
 	}
 
-	void Update() {
-        Movement();
-    }
+	private void SetCollisions() {
+		collisions = new Collisions[4];
+		collisions[0] = new Collisions(Vector2.left, false);
+		collisions[1] = new Collisions(Vector2.up, false);
+		collisions[2] = new Collisions(Vector2.right, false);
+		collisions[3] = new Collisions(Vector2.down, true);
+	}
 
-    private void Movement() {
+	void FixedUpdate() {
+		Movement();
+	}
+
+	private void Movement() {
 		Run();
 		Jump();
+		Fall();
+
+	}
+
+	private void CheckCollision() {
+
 	}
 
 	private void Run() {
 		float hInput = Input.GetAxis("Horizontal");
 		if (hInput != 0) {
-			if (IsColliding(hInput < 0 ? Vector3.left : Vector3.right)) {
+			float velocity = hInput * speed * Time.deltaTime;
+			Vector2 dir = hInput < 0 ? Vector3.left : Vector3.right;
+
+			RaycastHit2D ray = GetRaycast(dir);
+
+			if (ray.collider != null) {
+				if (!GetCollision(dir)) {
+					float movement = ray.distance - (transform.lossyScale.x / 2);
+					transform.position += new Vector3(movement, 0, 0);
+					SetCollisions(dir, true);
+				}
 				return;
+			} else {
+				SetCollisions(dir, false);
 			}
-			transform.position += new Vector3(hInput * speed * Time.deltaTime, 0, 0);
+
+			transform.position += new Vector3(velocity, 0, 0);
 		}
 	}
 
@@ -38,39 +68,94 @@ public class PlayerController : MonoBehaviour {
 			if (playerState != PlayerStates.JUMPING) {
 				playerState = PlayerStates.JUMPING;
 				jumpVelocity = jumpSpeed;
+				SetCollisions(Vector2.down, false);
 			}
 		}
 		if (playerState == PlayerStates.JUMPING) {
+			if (!GetCollision(Vector2.up)) {
+				RaycastHit2D ray = GetRaycast(Vector2.up);
+				if (ray.collider != null) {
+					jumpVelocity = 0;
+					SetCollisions(Vector2.up, true);
+				}
+			}
+		}
+	}
+
+	private void Fall() {
+		if (playerState == PlayerStates.JUMPING) {
+
 			jumpVelocity -= gravity * Time.deltaTime;
 			transform.position += new Vector3(0, jumpVelocity * Time.deltaTime, 0);
 
-			if (IsColliding(Vector2.up)) {
-				jumpVelocity = 0;
-			} 
-			else if (IsColliding(Vector2.down)) {
-				playerState = PlayerStates.IDLE;
+			RaycastHit2D ray = GetRaycast(Vector2.down);
+
+			if (ray.collider != null) {
+				if (!GetCollision(Vector2.down)) {
+					float movement = ray.distance - (transform.lossyScale.x / 2);
+					transform.position += new Vector3(0, -movement, 0);
+					SetCollisions(Vector2.down, true);
+					SetCollisions(Vector2.up, false);
+					playerState = PlayerStates.IDLE;
+				}
 				return;
 			}
 		}
 	}
 
-	private bool IsColliding(Vector2 dir) {
-		//Vector3 dir = input < 0 ? Vector3.left : Vector3.right;
+	/// <summary> Returns a raycast for the specified direction
+	/// </summary>
+	private RaycastHit2D GetRaycast(Vector2 dir) {
 		Vector3 origin = transform.position;
+		float distance = transform.lossyScale.x / 2 + 0.2f;
 
-		RaycastHit2D hit = Physics2D.Raycast(origin, dir, transform.lossyScale.x/2, collisionLayer);
-		Debug.DrawRay(origin, dir * transform.lossyScale.x/2, Color.yellow);
+		RaycastHit2D hit = Physics2D.Raycast(origin, dir, distance, collisionLayer);
+		Debug.DrawRay(origin, dir * distance, Color.yellow);
+		return hit;
+	}
+
+	/// <summary> Returns a bool of raycast collision success
+	/// </summary>
+	private bool IsColliding(Vector2 dir) {
+		Vector3 origin = transform.position;
+		float distance = transform.lossyScale.x / 2 + 0.2f;
+
+		RaycastHit2D hit = Physics2D.Raycast(origin, dir, distance, collisionLayer);
+		Debug.DrawRay(origin, dir * distance, Color.yellow);
 		return hit.collider != null;
-    }
+	}
 
-	private bool IsFalling() {
+	private bool GetCollision(Vector2 dir) {
+		for (int x = 0; x < collisions.Length; x++) {
+			if (collisions[x].dir == dir) {
+				return collisions[x].isColliding;
+			}
+		}
+		return false;
+	}
 
-		return true;
+	private void SetCollisions(Vector2 dir, bool state) {
+		for (int x = 0; x < collisions.Length; x++) {
+			if (collisions[x].dir == dir) {
+				collisions[x].isColliding = state;
+			}
+		}
 	}
 
 	private enum PlayerStates {
 		IDLE,
 		WALKING,
-		JUMPING
+		JUMPING,
+		FALLING
+	}
+
+	private struct Collisions {
+		public Vector2 dir;
+		public bool isColliding;
+
+		public Collisions(Vector2 dir, bool isColliding) {
+			this.dir = dir;
+			this.isColliding = isColliding;
+		}
 	}
 }
